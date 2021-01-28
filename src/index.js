064fed28b6144ab65a8123a75307e479ec0df680
Milok Murlyka <milok.murlika@yandex.ru>
2021-01-28 23:17:27 +0200

    fix DeprecationWarning http://bit.ly/2D8WfT6

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ const mongoose = require('mongoose');
 const defaultMongoOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true,
-  reconnectTries: Number.MAX_SAFE_INTEGER, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  bufferMaxEntries: 0, // If not connected, return errors immediately rather than waiting for reconnect
-  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  useUnifiedTopology: true
 };
 
 const middleware = (options = {}) => {
